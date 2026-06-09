7a93bae479df564507076b35296a1100e3e11b59
Noordeen <132381713+noordeen123@users.noreply.github.com>
2026-06-09 16:21:03 +0530

    feat: integrate Vercel Analytics into the main application root

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.tsx';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 );
